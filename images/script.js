e7eff84e8d374a8d31899bd14167d42e31ab087a
DKajuju <dianakajuju2@gmail.com>
2024-02-18 23:06:30 +0300

    add restaurant menu

---
+++ images/script.js
@@ -0,0 +1,13 @@
+let message = "Hello Zindua!";
+let num1= 3;
+let num2= 2;
+let shopping = ['bread', 'sugar', 'milk']
+let solution = num1 * num2
+let name = "Diana";
+
+let newMessage = `${message} my name is ${name}.`;
+let sum = num1 / num2;
+
+console.log(solution);
+console.log(shopping);
+console.log(newMessage);
